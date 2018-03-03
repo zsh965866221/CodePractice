51df6c91429f2cf6e33c4e4d155d362e5a15426f
zsh_o <965866221@qq.com>
2018-03-03 18:17:48 +0800

    hihocoder 1015

--- hihoCoder/1015.cpp
+++ hihoCoder/1015.cpp
@@ -3,13 +3,54 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <cassert>
 
 using namespace std;
 
-
+vector<int> GetNext(string s){
+    vector<int> next(s.size()+1,0);
+    next[0] = -1;
+    next[1] = 0;
+    for(int i=2;i<next.size();i++){
+        int k = next[i-1];
+        char c = s[i-1];
+        while(k>-1 && c!=s[k]){
+            k = next[k];
+        }
+        next[i] = k+1;
+    }
+    return next;
+}
+int search(string _s, string _t, vector<int> next){
+    int total = 0;
+    int n = 0;
+    int np = 0;
+    while(n<_s.size()){
+        while(np>0 && _s[n]!=_t[np]){
+            np = next[np];
+        }
+        if(_s[n]==_t[np])
+            np++;
+        n++;
+        if(np==_t.size()){
+            total++;
+            np = next[np];
+        }
+    }
+    return total;
+}
 
 int main(){
-    cout<<"test"<<endl;
+    int n;
+    cin>>n;
+    string s;
+    string t;
+    for(int i=0;i<n;i++){
+        cin>>t;cin>>s;
+        auto next = GetNext(t);
+        cout<<search(s, t, next)<<endl;
+    }
     cin.ignore();
     cin.get();
 }
